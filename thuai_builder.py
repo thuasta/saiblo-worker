e448e7fd7a3d92657553aa9575aba176887244b6
Spartan20859 <xy-l23@mails.tsinghua.edu.cn>
2025-01-28 09:20:46 +0800

    feat(thuai_builder): use code_id as image tag to avoid duplication

--- thuai_builder.py
+++ thuai_builder.py
@@ -8,8 +8,8 @@ import docker
 import docker.errors
 from base_docker_image_builder import BaseDockerImageBuilder
 
-BUILDER_NAME_LENGTH = 10
-IMAGE_NAME_PREFIX = "THUAI-image"
+# BUILDER_NAME_LENGTH = 10
+# IMAGE_NAME_PREFIX = "thuai-image-"
 
 
 class ThuaiBuilder(BaseDockerImageBuilder):
@@ -19,19 +19,25 @@ class ThuaiBuilder(BaseDockerImageBuilder):
         self.client = docker.from_env()
         self.built_images = {}
 
-        # Generate a random string for the builder, to avoid name conflict
-        chars = string.ascii_letters + string.digits
-        self.name = "".join(random.choice(chars) for _ in range(BUILDER_NAME_LENGTH))
+        # # Generate a random string for the builder, to avoid name conflict
+        # chars = string.ascii_letters + string.digits
+        # self.name = "".join(random.choice(chars) for _ in range(BUILDER_NAME_LENGTH))
 
-        # ID for image, to avoid name conflict
-        self.image_id = 0
+        # # ID for image, to avoid name conflict
+        # self.image_id = 0
 
-    async def build(self, file_path: Path) -> str:
+    async def build(self, file_path: Path, code_id: str) -> str:
+        # get all image tags
+        built_image_tags = [tag.split(':')[0] 
+                                for image in self.client.images.list() 
+                                for tag in image.tags]
+        # print(f"Built image tags: {built_image_tags}")
         # if not built yet...
-        if file_path not in self.built_images:
-            img_tag = self.get_image_name()
-            self.client.images.build(path=file_path, tag=img_tag, rm=True)
-            self.built_images[file_path] = img_tag
+        if code_id not in built_image_tags:
+            # print(f"Building image {code_id} from {file_path}")
+            self.client.images.build(path=str(file_path), tag=code_id, rm=True)
+        
+        self.built_images[file_path] = code_id
 
         return self.built_images[file_path]
 
@@ -43,14 +49,15 @@ class ThuaiBuilder(BaseDockerImageBuilder):
     async def list(self) -> Dict[Path, str]:
         return self.built_images.copy()
 
-    async def get_image_name(self) -> str:
-        """Get a no-duplicate name for images
+    # def get_image_name(self) -> str:
+    #     """Get a no-duplicate name for images
 
-        Name format: image-{builder name}-{id}
+    #     Name format: image-{builder name}-{id}
 
-        Returns:
-            The generated image name.
-        """
-        name = IMAGE_NAME_PREFIX + self.name + "-" + str(self.image_id)
-        self.image_id = self.image_id + 1
-        return name
+    #     Returns:
+    #         The generated image name.
+    #     """
+    #     # name = IMAGE_NAME_PREFIX + self.name + "-" + str(self.image_id)
+    #     name = IMAGE_NAME_PREFIX + "-" + str(self.image_id)
+    #     self.image_id = self.image_id + 1
+    #     return name
